60b106cd448aaf521fd576ec015ccd81e4bad678
A-Abd <abrar.abdulwahed@gmail.com>
2023-03-13 19:31:41 +0300

    feat: navbar in large devices

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
-
+import Navbar from '../src/components/Navbar'
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  )
 }
