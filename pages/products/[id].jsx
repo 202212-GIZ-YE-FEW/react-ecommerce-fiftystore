6dab69f5a104650c38982e065a4530e2036eefc5
Jehad Almaliki <yourgitemail@example.com>
2023-03-16 04:51:40 +0300

    feat : add item to cart and increase or decrease quantity

--- pages/products/[id].jsx
+++ pages/products/[id].jsx
@@ -1,9 +1,68 @@
 import { React } from "react";
+import { useState, useEffect } from 'react';
 import { Button } from "../../src/components/Button";
 import { getAllProducts } from '../../utils/API';
 import { StarIcon, Bars3CenterLeftIcon } from '@heroicons/react/24/outline';
 
 function SingleProduct({ product }) {
+
+    const [cartItems, setCartItems] = useState([]);
+  
+    // Retrieve cart items from local storage on page load
+    useEffect(() => {
+      const items = localStorage.getItem('cartItems');
+      if (items) {
+        setCartItems(JSON.parse(items));
+      }
+    }, []);
+
+    const addToCart = (item) => {
+        const itemInCart = cartItems.find((cartItem) => cartItem.id === item.id);
+        let updatedCartItems = [];
+        if (itemInCart) {
+          updatedCartItems = cartItems.map((cartItem) =>
+            cartItem.id === item.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
+          );
+        } else {
+          updatedCartItems = [...cartItems, { ...item, quantity: 1 }];
+        }
+        setCartItems(updatedCartItems);
+        // Save cart items to local storage
+        localStorage.setItem('cartItems', JSON.stringify(updatedCartItems));
+      };
+
+      const increaseQuantity = (id) => {
+        const itemIndex = cartItems.findIndex((item) => item.id === id);
+        if (itemIndex === -1) {
+          // Item not found in cart, add it with quantity 1
+          const newItem = { id, quantity: 1 };
+          const updatedCartItems = [...cartItems, newItem];
+          setCartItems(updatedCartItems);
+          localStorage.setItem('cartItems', JSON.stringify(updatedCartItems));
+        } else {
+          // Item found in cart, update its quantity
+          const updatedCartItems = [...cartItems];
+          updatedCartItems[itemIndex].quantity++;
+          setCartItems(updatedCartItems);
+          localStorage.setItem('cartItems', JSON.stringify(updatedCartItems));
+        }
+      };
+      const decreaseQuantity = (id) => {
+        const updatedCartItems = cartItems.map((item) => {
+          if (item.id === id && item.quantity > 1) {
+            return {
+              ...item,
+              quantity: item.quantity - 1,
+            };
+          }
+          return item;
+        });
+        setCartItems(updatedCartItems);
+        localStorage.setItem('cartItems', JSON.stringify(updatedCartItems));
+      };
+     
+      
+
     return (
         <div className="flex items-center justify-center m-10">
             <img className="object-fill h-100 w-96" src={product.image} alt="" />
@@ -20,8 +79,24 @@ function SingleProduct({ product }) {
                     <StarIcon className="object-fill h-5 w-5 text-orange-400" />
                 </div>
                 <div className="flex flex-wrap">
-                    <input type="number" defaultValue={1} className="sm:text-center border border-gray-300 rounded w-10 bg-gray-50 mb-1 font-medium text-gray-900 mr-5" />
-                    <Button className="btn" content={"Add To Cart"} />
+                <button
+                   onClick={() => decreaseQuantity(product.id)}
+
+                  className="bg-gray-200 text-gray-700 px-2 py-1 rounded-md focus:outline-none"
+                >
+                  -
+                </button>
+        
+                <span className="px-2 text-gray-700"> </span>
+          
+                <button
+                  onClick={() => increaseQuantity(product.id)}
+                  className="bg-gray-200 text-gray-700 px-2 py-1 rounded-md focus:outline-none"
+                >
+                  +
+                </button>
+                
+                    <Button className="btn" content={"Add To Cart"}  handleClickFun={() => addToCart(product)} />
                 </div>
             </div>
         </div>
