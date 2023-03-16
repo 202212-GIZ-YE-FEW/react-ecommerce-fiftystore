484ea7bf7884407d483766d7d075c55695ba8633
Jehad Almaliki <yourgitemail@example.com>
2023-03-16 04:52:23 +0300

    feat : add handleFun on button as props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,10 @@
-export const Button = ({ content, href=null, additional_style=""}) => {
+export const Button = ({ content, href=null, additional_style="" ,handleClickFun=null }) => {
     return (
         <>
-            {href ? <a href="#" className={`inline-block cursor-pointer font-semibold px-6 py-2 rounded-full uppercase bg-stone-900 text-white hover:bg-red-700 ${additional_style}`}>{ content }</a> : 
-            <button className={`inline-block cursor-pointer font-semibold px-6 py-2 rounded-full uppercase bg-stone-900 text-white hover:bg-red-700 ${additional_style}`}>{content}</button> }            
+            {href ? <a  className={`inline-block cursor-pointer font-semibold px-6 py-2 rounded-full uppercase bg-stone-900 text-white hover:bg-red-700 ${additional_style}`}>{ content }</a> : 
+            <button 
+            onClick={handleClickFun}
+            className={`inline-block cursor-pointer font-semibold px-6 py-2 rounded-full uppercase bg-stone-900 text-white hover:bg-red-700 ${additional_style}`}>{content}</button> }            
         </>
     )
 }
