de49ad8e3c20e48b6f5761ac500755120f944836
A-Abd <abrar.abdulwahed@gmail.com>
2023-03-18 23:26:09 +0300

    refactor

--- pages/products/index.js
+++ pages/products/index.js
@@ -11,22 +11,9 @@ import { Product } from "../../src/components/Product"
 
 
 export default function Products({ products, categories }) {
-const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
-const [ productsState, setProductsState ] = useState(products);
-const [ categoryFilter , setCategoryFilter ] = useState([]);
+    const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
+    const [ categoryFilter , setCategoryFilter ] = useState([]);
 
-
-function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
-}
-
-const sortOptions = [
-    
-    { name: 'Best Rating', href: '#', current: false },
-    { name: 'Newest', href: '#', current: false },
-    { name: 'Price: Low to High', href: '#', current: false },
-    { name: 'Price: High to Low', href: '#', current: false },
-    ]
     const filters = [
     {
         id: 'category',
@@ -35,12 +22,6 @@ const sortOptions = [
         options: categories.map(category => {
             return {name: category.trim(), value: category.replace(/\s+/g, '-').toLowerCase(), label: category, checked:false}
         }) 
-    },
-    {
-        id: 'price',
-        name: 'Price',
-        type: "range",
-        options: [],
     },
     ]
 
@@ -55,7 +36,7 @@ const sortOptions = [
             newCategoryItem.splice(currentIndex, 1);
         setCategoryFilter(newCategoryItem);
     };
-    const handlePriceChange = value => {
+    const handleSortByChange = value => {
         // const currentIndex = categoryFilter.indexOf(value);
         // const newChecked = [...categoryFilter];
         // if(currentIndex === -1)
@@ -124,7 +105,7 @@ const sortOptions = [
                                             <div key={option.value} className="flex items-center">
                                                 <input
                                                 name={`${section.id}[]`}
-                                                type={`${section.type}`}
+                                                type="checkbox"
                                                 checked={categoryFilter.indexOf(option.label) === -1? false:true}
                                                 onChange={() => handleCategoryChange(option.label)}
                                                 className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
@@ -152,47 +133,6 @@ const sortOptions = [
                 <main className="mx-auto max-w-8xl px-4 sm:px-6 lg:px-8">
                     <div className="flex items-baseline justify-between border-b border-gray-200 pt-6 pb-6">
                         <div className="flex items-center">
-                            <Menu as="div" className="relative inline-block text-left">
-                                <div>
-                                <Menu.Button className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
-                                    Sort
-                                    <ChevronDownIcon
-                                    className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
-                                    aria-hidden="true"
-                                    />
-                                </Menu.Button>
-                                </div>
-                                <Transition
-                                as={Fragment}
-                                enter="transition ease-out duration-100"
-                                enterFrom="transform opacity-0 scale-95"
-                                enterTo="transform opacity-100 scale-100"
-                                leave="transition ease-in duration-75"
-                                leaveFrom="transform opacity-100 scale-100"
-                                leaveTo="transform opacity-0 scale-95"
-                                >
-                                <Menu.Items className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                    <div className="py-1">
-                                    {sortOptions.map((option) => (
-                                        <Menu.Item key={option.name}>
-                                        {({ active }) => (
-                                            <a
-                                            href={option.href}
-                                            className={classNames(
-                                                option.current ? 'font-medium text-gray-900' : 'text-gray-500',
-                                                active ? 'bg-gray-100' : '',
-                                                'block px-4 py-2 text-sm'
-                                            )}
-                                            >
-                                            {option.name}
-                                            </a>
-                                        )}
-                                        </Menu.Item>
-                                    ))}
-                                    </div>
-                                </Menu.Items>
-                                </Transition>
-                            </Menu>
                             <button
                                 type="button"
                                 className="-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden"
@@ -245,7 +185,7 @@ const sortOptions = [
                             {/* Product grid */}
                             <div className="lg:col-span-3">
                                 <div className="mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                                   {productsState.filter((item) => { //Use the filter to compare the value of the input (will take the value as props from navbar component), so that if it is empty, it displays all the data, and if any character changes, it displays the data that includes this character
+                                   {products.filter((item) => { //Use the filter to compare the value of the input (will take the value as props from navbar component), so that if it is empty, it displays all the data, and if any character changes, it displays the data that includes this character
                                     if (categoryFilter.length === 0) {
                                         return item;
                                     }
--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 export const SearchResults = ({ searchResults }) => {
     return (
         <>
-            <div className="flex justify-between items-center max-w-[1520px] w-3/4 absolute top-20 left-1/2 -translate-x-1/2 rounded-md bg-white py-3 px-4 z-10 shadow-lg">
+            <div className="flex justify-between items-center w-full w-3/4 absolute top-20 left-1/2 -translate-x-1/2 rounded-md bg-white py-3 px-4 z-10 shadow-lg">
                 <ul role="list" className="w-full divide-y divide-gray-200 dark:divide-gray-700">
                     {searchResults.map((res) => 
                         <li className="py-3 sm:py-4 hover:bg-gray-300" key={res.id}>
