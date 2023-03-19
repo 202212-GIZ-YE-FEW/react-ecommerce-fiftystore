624b18e4231bac9825d46f537d1a7c242467d2c1
A-Abd <abrar.abdulwahed@gmail.com>
2023-03-19 17:54:16 +0300

    refactor: remove unrelated lines

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,14 +5,9 @@ import Footer from '../components/Footer'
 import { SearchProvider } from '../context/SearchContext'
 
 export default function App({ Component, pageProps }) {
-  // Get menu data via GraphQL query in `pageProps`.
-  const {menu} = pageProps
-
-  // Store menu as state for the MenuProvider.
-  const [navMenu] = useState(menu)
   return (
     <>
-      <SearchProvider value={navMenu}>
+      <SearchProvider>
         <Navbar />
         <Component {...pageProps} />
         <Footer />
